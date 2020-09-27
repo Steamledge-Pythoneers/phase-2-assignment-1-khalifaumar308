2b9caa3b0d12d1b8428befc87cc6b8b71c2d1243
Umar Muhammad Aminu <khalifaumar308@gmail.com>
2020-09-27 22:44:27 +0100

    add docstrings to lowest_terms function

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,26 +1,30 @@
 ## TODO: complete the function "lowest_terms" below
 
 def lowest_terms(x):
-    x=x.split('/')#split x into a list
-    n=int(x[0])#assign n as the numerator
-    d=int(x[1])#assign d as the denomerator
-    sign=''#assign an empty string as the defult sign
-    if n<0 and d<0:#check if both n and d are negative
-        sign=''
-    elif n<0 or d<0:#check if either n or d is negative
-        sign='-'    
-    if n==0:#assert that no ZeroDivisionError
+    """A function that accepts a fraction as input,
+    compute and returns the lowest 
+    possible form of the fraction"""
+    x = x.split('/')#split x into a list
+    numerator = int(x[0])#assign n as the numerator
+    denomenator = int(x[1])#assign d as the denomerator
+    sign = ''#assign an empty string as the defult sign
+    if numerator < 0 and denomenator <0:#check if both n and d are negative
+        sign = ''
+    elif numerator < 0 or denomenator <0:#check if either n or d is negative
+        sign = '-'    
+    if numerator == 0:#assert that no ZeroDivisionError
         return '0'
-    if d==0:#assert that no ZeroDivisionError
+    if denomenator == 0:#assert that no ZeroDivisionError
         return 'Undefined'    
-    n=(n**2)**0.5#make n positive
-    d=(d**2)**0.5
+    numerator = (numerator**2)**0.5#make n positive
+    denomenator = (denomenator**2)**0.5
     #assign the values of n and d to x/y to find hcf
-    x=n
-    y=d
-    while y!=0:#find the hcf of n and d
+    x = numerator #copy the value of the numerator
+    y = denomenator #copy the value of denomenator
+    #find the hcf of n and d
+    while y != 0:
         i=y
         y=x%y
         x=i
-    return '{}{}/{}'.format(sign,int(n/x),int(d/x))      
+    return '{}{}/{}'.format(sign,int(numerator/x),int(denomenator/x))      
 
